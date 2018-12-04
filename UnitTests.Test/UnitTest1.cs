4df5d329dd863fb303824db9bf84167f4281870c
mkora <31717889+mkora@users.noreply.github.com>
2018-12-04 12:45:48 -0800

    Add description of unit tests & collections

--- UnitTests.Test/UnitTest1.cs
+++ UnitTests.Test/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace UnitTests.Test
             /**
              * A collection of helper classes to test various conditions within unit tests:
              * 
-             * Class Assert. Methods:
+             * Class Assert. Methods (Namespace: Microsoft.VisualStudio.TestTools.UnitTesting):
              * 
              * AreEqual                 Tests whether the specified inputs are equal and throws an exception if they are not equal.
              * 
@@ -67,6 +67,49 @@ namespace UnitTests.Test
              * 
              * Source:
              * https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert?redirectedfrom=MSDN&view=mstest-net-1.2.0#methods
+             *
+             * ----------------------------------------------------------------------------------------------------------------------
+             *
+             * Class CollectionAssert Methods (Namespace: Microsoft.VisualStudio.TestTools.UnitTesting):
+             * 
+             * AllItemsAreInstancesOfType       Tests whether all elements in the specified collection are instances of the expected type 
+             *                                  and throws an exception if the expected type is not in the inheritance hierarchy of 
+             *                                  one or more of the elements.
+             *                                                  
+             * AllItemsAreNotNull               Tests whether all items in the specified collection are non-null and throws an exception
+             *                                  if any element is null.
+             *                                                  
+             * AllItemsAreUnique                Tests whether all items in the specified collection are unique or not and throws if any 
+             *                                  two elements in the collection are equal.
+             *                                                 
+             * AreEqual                         Tests whether the specified collections are equal and throws an exception if the two 
+             *                                  collections are not equal. Equality is defined as having the same elements in the same order 
+             *                                  and quantity. Different references to the same value are considered equal.
+             *       
+             * AreEquivalent                    Tests whether two collections contain the same elements and throws an exception 
+             *                                  if either collection contains an element not in the other collection.
+             *                                  
+             * AreNotEqual                      Tests whether the specified collections are unequal and throws an exception if the 
+             *                                  two collections are equal. Equality is defined as having the same elements in the same order 
+             *                                  and quantity. Different references to the same value are considered equal.
+             * 
+             * AreNotEquivalent                 Tests whether two collections contain the different elements and throws an exception 
+             *                                  if the two collections contain identical elements without regard to order.
+             *                                  
+             * Contains                         Tests whether the specified collection contains the specified element and throws an exception
+             *                                  if the element is not in the collection.
+             *                                  
+             * DoesNotContain                   Tests whether the specified collection does not contain the specified element and 
+             *                                  throws an exception if the element is in the collection.
+             *                                  
+             * IsNotSubsetOf                    Tests whether one collection is not a subset of another collection and throws an exception
+             *                                  if all elements in the subset are also in the superset.
+             *                                  
+             * IsSubsetOf                       Tests whether one collection is a subset of another collection and throws an exception
+             *                                  if any element in the subset is not also in the superset.
+             * 
+             * Source:
+             * https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.collectionassert?redirectedfrom=MSDN&view=mstest-net-1.2.0
              */
 
             int[] vals = { 1, 2, 3, 4, 5 };
